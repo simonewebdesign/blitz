fbc2fc837645bfce89274a984207243766c0fbbb
Simone <hello@simonewebdesign.it>
2024-03-10 22:39:17 +0000

    wat

--- lib/blitz_web/controllers/watching_controller.ex
+++ lib/blitz_web/controllers/watching_controller.ex
@@ -14,10 +14,10 @@ defmodule BlitzWeb.WatchingController do
     render(conn, :new, changeset: changeset)
   end
 
-  def create(conn, %{"watching" => watching_params}) do
-    watching_params = Map.put(watching_params, "user_id", "14c59c04-2a6a-4b8f-8b7d-dfe91f50e2e6")
+  def create(conn, %{"watching" => wat_params}) do
+    wat_params = Map.put(wat_params, "user_id", "14c59c04-2a6a-4b8f-8b7d-dfe91f50e2e6")
 
-    case Core.create_watching(watching_params) do
+    case Core.create_watching(wat_params) do
       {:ok, wat} ->
         # Start a background job to fetch the data
         # TODO: put it under a supervision tree
@@ -29,7 +29,7 @@ defmodule BlitzWeb.WatchingController do
         # Redirect to the newly created watching
         conn
         |> put_flash(:info, "Watching created successfully.")
-        |> redirect(to: ~p"/watchings/#{watching}")
+        |> redirect(to: ~p"/watchings/#{wat}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, :new, changeset: changeset)
@@ -37,33 +37,33 @@ defmodule BlitzWeb.WatchingController do
   end
 
   def show(conn, %{"id" => id}) do
-    watching = Core.get_watching!(id)
-    render(conn, :show, watching: watching)
+    wat = Core.get_watching!(id)
+    render(conn, :show, watching: wat)
   end
 
   def edit(conn, %{"id" => id}) do
-    watching = Core.get_watching!(id)
-    changeset = Core.change_watching(watching)
-    render(conn, :edit, watching: watching, changeset: changeset)
+    wat = Core.get_watching!(id)
+    changeset = Core.change_watching(wat)
+    render(conn, :edit, watching: wat, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "watching" => watching_params}) do
-    watching = Core.get_watching!(id)
+  def update(conn, %{"id" => id, "watching" => wat_params}) do
+    wat = Core.get_watching!(id)
 
-    case Core.update_watching(watching, watching_params) do
-      {:ok, watching} ->
+    case Core.update_watching(wat, wat_params) do
+      {:ok, wat} ->
         conn
         |> put_flash(:info, "Watching updated successfully.")
-        |> redirect(to: ~p"/watchings/#{watching}")
+        |> redirect(to: ~p"/watchings/#{wat}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, watching: watching, changeset: changeset)
+        render(conn, :edit, watching: wat, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    watching = Core.get_watching!(id)
-    {:ok, _watching} = Core.delete_watching(watching)
+    wat = Core.get_watching!(id)
+    {:ok, _wat} = Core.delete_watching(wat)
 
     conn
     |> put_flash(:info, "Watching deleted successfully.")
