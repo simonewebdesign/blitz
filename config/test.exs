70c925fce4cd1e4fa67ac8b7306f91c7eafdd93f
Simone <hello@simonewebdesign.it>
2024-03-10 19:31:12 +0000

    Update config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :blitz, Blitz.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "simo",
+  password: "",
   hostname: "localhost",
   database: "blitz_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
